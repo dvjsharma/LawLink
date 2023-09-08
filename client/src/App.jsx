8b01304bfe2936cf6a1081a2b3e4e76c7abea718
Akash Kumar Sah <akashsah2003@gmail.com>
2023-09-08 20:27:08 +0530

    Home page setup+Navbar setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,12 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import {Routes, Route} from 'react-router-dom';
 import About from './Pages/About';
+import Home from './Pages/Home';
 function App() {
   return (
     <Routes>
       <Route path='/about' element={<About/>}></Route>
+      <Route path='/' element={<Home/>}></Route>
     </Routes>
   )
 }
