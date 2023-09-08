2334a629a8af59ae8c930477b9c0725f938721f4
Akash Kumar Sah <akashsah2003@gmail.com>
2023-09-08 22:53:06 +0530

    Navbar Done

--- client/src/Components/Navbar/index.jsx
+++ client/src/Components/Navbar/index.jsx
@@ -1,37 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Hamburger from 'hamburger-react'
+
 
 const Navbar=()=>{
+    const [isOpen, setOpen] = useState(false);
     return (
-        <nav className="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <a href="#" className="flex items-center">
-        <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
-        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-    </a>
-    <button data-collapse-toggle="navbar-solid-bg" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-solid-bg" aria-expanded="false">
-        <span className="sr-only">Open main menu</span>
-        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-        </svg>
-    </button>
-    <div className="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
-      <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
-        <li>
-          <a href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">Home</a>
-        </li>
-        <li>
-          <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
-        </li>
-        <li>
-          <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
-        </li>
-        <li>
-          <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+      <>
+        <div className="flex items-center justify-center bg-[#12151b] text-[#f7f8f9] w-full text-md p-2 lg:flex hidden ">
+          <div className="flex items-center justify-center w-[20%] text-3xl">
+            <h1>TEST</h1>
+          </div>
+          <div className="flex items-center justify-center gap-x-20 w-[60%]">
+            <Link to="/">Home</Link>
+            <Link to="/">Find Clients</Link>
+            <Link to="/">Hire Providers</Link>
+            <Link to="/">About</Link>
+          </div>
+          <div className="flex items-center justify-center w-[20%] gap-x-20">
+            <Link to="/">Login</Link>
+            <Link to="/">Sign up</Link>
+            
+          </div>
+        </div>
+        <div className="flex items-center justify-center bg-[#12151b] text-[#f7f8f9] w-full text-md p-2 lg:hidden flex ">
+          <div className="flex items-center justify-center w-[20%] text-3xl">
+            <Hamburger toggled={isOpen} toggle={setOpen}/>
+          </div>
+          <div className="flex items-center justify-center w-[60%]">
+            <h1>TEST2</h1>
+          </div>
+          <div className="flex items-center justify-center w-[20%] gap-x-6 md:gap-x-20">
+            <Link to="/">Login</Link>
+            <Link to="/">Sign up</Link>
+            
+          </div>
+        </div>
+        <div className={`absolute w-60 gap-y-2 flex items-center justify-center flex-col bg-[#12151b] text-[#f7f8f9] text-md p-2 lg:hidden flex top-[62px] ${isOpen ? 'flex' : 'hidden'}`}>
+            <Link to="/">Home</Link>
+            <hr />
+            <Link to="/">Find Clients</Link>
+            <hr />
+            <Link to="/">Hire Providers</Link>
+            <hr />
+            <Link to="/">About</Link>
+            <hr />
+          
+        </div>
+      </>
     );
 };
 export default Navbar;
