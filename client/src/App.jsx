e319f777c3f1a8e83298da7cf920976600905400
RISHIKESHk07 <rishikeshkuppala@gmail.com>
2023-09-11 23:32:56 +0530

    added a login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,14 @@ import './App.css'
 import {Routes, Route} from 'react-router-dom';
 import About from './Pages/About';
 import Home from './Pages/Home';
-import Footer from './Components/Footer';
+import Login from './Pages/Login';
+
 function App() {
   return (
     <Routes>
       <Route path='/about' element={<About/>}></Route>
       <Route path='/' element={<Home/>}></Route>
+      <Route path='/login' element={<Login/>}></Route>
     </Routes>
   )
 }
